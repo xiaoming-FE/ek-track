6487a2d3346469dbc82f5167b4484aacdfef4b6d
unknown <h5cool@163.com>
2019-02-25 15:56:23 +0800

    new constructor

---
+++ README.md
@@ -0,0 +1,13 @@
+## use 
+```
+npm install ek-track
+```
+
+## example
+```
+cd example 
+
+npm install 
+
+npm run dev
+```
---
+++ example/test.js
@@ -0,0 +1,8 @@
+import track from '../index.js';
+track.init({url:"http://locahost:3000"});
+// 主动触发按钮post数据
+document.querySelector("#active").onclick=function(event){
+    track.send({event:event,sign:"hahaha",data:{name:1,age:2}})
+}
+
+// 不触发主动发送，比如异常上报
